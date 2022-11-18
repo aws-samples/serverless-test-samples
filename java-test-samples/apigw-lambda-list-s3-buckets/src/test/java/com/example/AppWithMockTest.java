274b03bb28093cf879f28055b23d2e46fcfd3152
poprahul <poprahul@amazon.com>
2022-11-18 11:04:07 -0500

    Incorporated review comments to use java-test-lib events

--- java-test-samples/apigw-lambda-list-s3-buckets/src/test/java/com/example/AppWithMockTest.java
+++ java-test-samples/apigw-lambda-list-s3-buckets/src/test/java/com/example/AppWithMockTest.java
@@ -5,14 +5,22 @@
 
 package com.example;
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.tests.annotations.Event;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -29,13 +37,15 @@ public class AppWithMockTest {
     lenient().when(s3Client.listBuckets()).thenReturn(ListBucketsResponse.builder().buckets(bucket).build());
   }
 
-  @Test
-  public void successfulResponse() {
+  @ParameterizedTest
+  @Event(value = "events/apigw_req_s3_buckets_get.json", type = APIGatewayProxyRequestEvent.class)
+  public void successfulGetResponse(APIGatewayProxyRequestEvent event) {
     App app = new App(s3Client);
-    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
-    assertEquals(200, result.getStatusCode().intValue());
-    assertEquals("text/plain", result.getHeaders().get("Content-Type"));
-    String content = result.getBody();
+    APIGatewayProxyResponseEvent response = app.handleRequest(event, null);
+    Assertions.assertNotNull(response);
+    assertEquals(HttpStatus.SC_OK, response.getStatusCode().intValue());
+    assertEquals("text/plain", response.getHeaders().get("Content-Type"));
+    String content = response.getBody();
     assertNotNull(content);
     assertTrue(content.length() > 0);
     assertEquals("foo", content);
