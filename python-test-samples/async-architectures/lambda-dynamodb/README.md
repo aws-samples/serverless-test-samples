42281f23d7761f8db1f540594df79328c76978d0
Dan Fox <dfxamzn@amazon.com>
2023-01-12 12:41:36 -0700

    make checkpoint commit

--- python-test-samples/async-architectures/lambda-dynamodb/README.md
+++ python-test-samples/async-architectures/lambda-dynamodb/README.md
@@ -3,4 +3,4 @@ You may use a variety of resource types to create the event listener for your as
 
 In this pattern, a Lambda function is configured to be an event listener to receive the SUT’s output data. DynamoDB is configured to be the event listener storage option. In Step 1 in the diagram below, the test establishes a long polling pattern with DynamoDB. In Step 2, the test sends input data to the asynchronous system under test. The async system processes the data and in Step 3 the the Lambda function receives the data and puts it into DynamoDB. The long poll queries DynamoDB and examines the result. We recommend writing tests for failure conditions including timeouts and malformed requests.
 
-![AWS Lambda and AmazonDynamoDB](./img/lambda-dynamo.png)
+![AWS Lambda and AmazonDynamoDB](../img/lambda-dynamo.png)
--- python-test-samples/async-architectures/lambda-dynamodb/tests/integration/test_async_transform.py
+++ python-test-samples/async-architectures/lambda-dynamodb/tests/integration/test_async_transform.py
@@ -4,24 +4,42 @@
 import os
 import uuid
 from unittest import TestCase
-import time
 import boto3
 import requests
 from datetime import datetime, timedelta
 
 """
-Set the environment variable AWS_SAM_STACK_NAME 
-to match the name of the stack you will test
-AWS_SAM_STACK_NAME=async-test-pattern python -m pytest -s tests/integration -v
+usage:
+
+Set the environment variable AWS_SAM_STACK_NAME to match the name of the stack you will test
+-- or -- 
+add AWS_SAM_STACK_NAME to the beginning of your command
+
+example:
 AWS_SAM_STACK_NAME=<stack-name> python -m pytest -s tests/integration -v
 """
 
 class TestAsynchronousTransformation(TestCase):
 
-    poll_timeout_duration_secs = 5 
+    """
+    Asynchronous systems may have service level agreements (SLA's) for the latency of a 
+    given process. When you test the system, you can use this SLA to set the timeout 
+    duration for your test. If you do not have an explicit SLA, you may set the timeout 
+    duration to be some reasonable amount that you would expect the system to complete 
+    the process.    
+    """
+
+    # set timeout duration to your SLA guarantee
+    poll_timeout_duration_secs = 10 
+    
+    # set a random filename
     test_filename = str(uuid.uuid4()) + ".txt"
-    lower_case_message = "this message was created during an integration test"
-    upper_case_message = "THIS MESSAGE WAS CREATED DURING AN INTEGRATION TEST"
+
+    # the file initially contains a lowercase string
+    unmodified_message = "this message was created during an integration test"
+
+    # the string will be transformed to uppercase by the end of the process 
+    modified_message = "THIS MESSAGE WAS CREATED DURING AN INTEGRATION TEST"
 
     @classmethod
     def getStackName(cls) -> str:
@@ -29,15 +47,16 @@ class TestAsynchronousTransformation(TestCase):
         if not stack_name:
             raise Exception(
                 "Cannot find env var AWS_SAM_STACK_NAME. \n"
-                "Please setup this environment variable with the stack name where we are running integration tests."
+                "Please include the stack name when running integration tests."
             )
 
         return stack_name
 
     def setUp(self) -> None:
         """
-        Based on the provided env variable AWS_SAM_STACK_NAME,
-        here we use CloudFormation API to find out the SourceBucket name 
+        Based on the provided AWS_SAM_STACK_NAME, we can use CloudFormation API to find out the S3 SourceBucket name
+        where the initial data will be put, as well as the DynamoDB TableName where the modified data will eventually reside 
+        at the end of the process.
         """
         stack_name = TestAsynchronousTransformation.getStackName()
 
@@ -59,28 +78,33 @@ class TestAsynchronousTransformation(TestCase):
 
         stack_outputs = stacks[0]["Outputs"]
         async_test_results_output = [output for output in stack_outputs if output["OutputKey"] == "AsyncTransformTestResultsTable"]
-        self.assertTrue(async_test_results_output, f"Cannot find output AsyncTransformTestResults in stack {stack_name}")
+        self.assertTrue(async_test_results_output, f"Cannot find output AsyncTransformTestResultsTable in stack {stack_name}")
         self.async_test_results_table = async_test_results_output[0]["OutputValue"]
 
     def testPutObjectIntoSourceBucket(self):
         """
-        Put an object into the source bucket
+        Put an unmodified, lowercase string into the source bucket. This the is beginning of the async process. 
         """
+        print()
+        print("Putting unmodified object into S3...")
         client = boto3.client('s3')
-        response = client.put_object(Body=self.lower_case_message, Bucket=self.source_bucket, Key=self.test_filename)
+        response = client.put_object(Body=self.unmodified_message, Bucket=self.source_bucket, Key=self.test_filename)
         self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], requests.codes.ok, f"Cannot put object into Source Bucket {self.source_bucket}")
 
     def testRetrieveObjectFromDynamoDB(self):
         """
-        Poll DynamoDB to retrieve object from end of async process
+        Poll to retrieve the modified string from a DynamoDB Table. This is the end of the async process.
         """    
-        print() # output neatness
+        print()
+        
+        # set the maximum amount of time to poll before the test fails
+        # this can be based on our system's SLA
         loop_start_time = datetime.now()
         loop_max_time = loop_start_time + timedelta(seconds=self.poll_timeout_duration_secs)
 
         while True:
 
-            # query dynamodb
+            # query dynamodb for the output
             dynamodb = boto3.client("dynamodb")
             response = dynamodb.get_item(
                 TableName=self.async_test_results_table,
@@ -92,10 +116,10 @@ class TestAsynchronousTransformation(TestCase):
             # did we get the object?
             try: 
                 x = response['Item']['message']
-                print("Queried DynamoDB for object. Success!")
+                print("*** Modified object found in DynamoDB. Success! ***")
 
                 # is the object formed correctly?
-                self.assertTrue(x['S'] == self.upper_case_message, f"Object malformed. Found {x}. Should be: {self.upper_case_message}")
+                self.assertTrue(x['S'] == self.modified_message, f"Object malformed. Found {x}. Should be: {self.modified_message}")
                 
                 # if yes, let's get out of here
                 break
@@ -103,7 +127,7 @@ class TestAsynchronousTransformation(TestCase):
             except KeyError:
                 pass
             
-            print("Queried DynamoDB for object. Not found yet...")
+            print("Querying DynamoDB for modified object. Not found yet...")
             
             if (loop_max_time < datetime.now()):
                 self.fail("Could not retrieve object from DynamoDB before timeout. Async process failed.")
