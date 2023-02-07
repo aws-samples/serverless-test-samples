c96246f054b5a9c6dfd514ec7fe3d09c85e3c50f
Brian Krygsman <krygsman@amazon.com>
2023-02-07 10:54:18 -0500

    Move unit tests next to code under test for now.

--- typescript-test-samples/typescript-sample-app/jest.config.js
+++ typescript-test-samples/typescript-sample-app/jest.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>','<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
