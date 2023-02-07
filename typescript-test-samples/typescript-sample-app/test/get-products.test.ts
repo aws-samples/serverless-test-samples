c835883e1949d3d02e58070965166345ac698efa
Brian Krygsman <krygsman@amazon.com>
2023-02-07 10:26:36 -0500

    Add unit tests for happy path and sad path, checking status codes.
    Implements mocking of class dependency.

--- typescript-test-samples/typescript-sample-app/test/get-products.test.ts
+++ typescript-test-samples/typescript-sample-app/test/get-products.test.ts
@@ -20,16 +20,74 @@
  *
  * Standard practice to put imports at the top of a module definition.
  */
-import { beforeEach, describe, it, expect } from '@jest/globals';
+import { beforeEach, describe, it, expect, jest } from '@jest/globals';
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import apiGWProxyEventStub from '../events/unit-test-event.json';
+import { logger } from '../src/powertools/utilities';
+
+import { DynamoDbStore } from "../src/store/dynamodb/dynamodb-store";
+
+jest.mock('../src/store/dynamodb/dynamodb-store');
+
+const mockedStore = jest.mocked(DynamoDbStore);
+
 import { handler } from '../src/api/get-products';
 
 describe( 'get-products', () => {
 
   describe( 'lambdaHandler()', () => {
 
+    let inputEvent: APIGatewayProxyEvent;
+    let inputContext: Context;
+
+    beforeEach(() => {
+      inputEvent = apiGWProxyEventStub;
+      inputContext = {
+        callbackWaitsForEmptyEventLoop: false,
+        functionName: '',
+        functionVersion: '',
+        invokedFunctionArn: '',
+        memoryLimitInMB: '',
+        awsRequestId: '',
+        logGroupName: '',
+        logStreamName: '',
+        getRemainingTimeInMillis: () => 1000,
+        done: function (error?: Error | undefined, result?: any): void {
+          throw new Error('Function not implemented.');
+        },
+        fail: function (error: string | Error): void {
+          throw new Error('Function not implemented.');
+        },
+        succeed: function (messageOrObject: any): void {
+          throw new Error('Function not implemented.');
+        }
+      };
+    });
+
     it('exists', () => {
       expect(handler).toBeTruthy();
-    })
+    });
+
+    it('returns 200 when finding products', async () => {
+      mockedStore.prototype.getProducts.mockImplementationOnce(() => Promise.resolve([]));
+      
+      const result = await handler(inputEvent, inputContext);
+      expect(mockedStore).toHaveBeenCalled();
+      expect(mockedStore.prototype.getProducts).toHaveBeenCalled();
+      expect(result.statusCode).toBe(200);
+    });
+
+    it('returns 500 when getting an error from the store', async () => {
+      mockedStore.prototype.getProducts.mockImplementationOnce(() => {
+        throw new Error('Test Error');
+      });
+      
+      const result = await handler(inputEvent, inputContext);
+      expect(mockedStore).toHaveBeenCalled();
+      expect(mockedStore.prototype.getProducts).toHaveBeenCalled();
+      expect(result.statusCode).toBe(500);
+    });
 
   });
 
