86388116f20d09ad1d44116edb0b9f9ac8019415
Chris Martinez <chmrtnz@amazon.com>
2023-02-13 15:40:56 -0800

    Support cancellation with upsert option for PUT

--- dotnet-test-samples/apigw-lambda-ddb/src/ServerlessTestApi.Core/Models/ProductDTO.cs
+++ dotnet-test-samples/apigw-lambda-ddb/src/ServerlessTestApi.Core/Models/ProductDTO.cs
@@ -8,9 +8,9 @@ public record ProductDTO
 
     public ProductDTO(string id, string name, decimal price)
     {
-        this.Id = id;
-        this.Name = name;
-        this.Price = price;
+        Id = id;
+        Name = name;
+        Price = price;
     }
         
     public string Id { get; init; }
