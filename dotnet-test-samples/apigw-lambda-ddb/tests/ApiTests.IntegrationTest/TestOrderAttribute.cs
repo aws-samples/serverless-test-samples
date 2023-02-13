d72a42157318d1ddcba098f114086b39dc363161
Chris Martinez <chmrtnz@amazon.com>
2023-02-13 15:49:56 -0800

    Refactor to AAA format and use ordered tests for dependent sequences

---
+++ dotnet-test-samples/apigw-lambda-ddb/tests/ApiTests.IntegrationTest/TestOrderAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTests.IntegrationTest;
+
+// REF: https://github.com/xunit/samples.xunit/blob/main/TestOrderExamples/TestCaseOrdering/TestPriorityAttribute.cs
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public sealed class TestOrderAttribute : Attribute
+{
+    public TestOrderAttribute(int order) => Order = order;
+
+    public int Order { get; }
+}
