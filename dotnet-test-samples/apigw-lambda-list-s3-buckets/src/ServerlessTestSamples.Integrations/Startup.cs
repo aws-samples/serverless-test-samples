fdfa76acbb67ea56a45d435e20e3aa35b6380843
Chris Martinez <chmrtnz@amazon.com>
2023-02-13 17:55:17 -0800

    Support cancellation and add options

--- dotnet-test-samples/apigw-lambda-list-s3-buckets/src/ServerlessTestSamples.Integrations/Startup.cs
+++ dotnet-test-samples/apigw-lambda-list-s3-buckets/src/ServerlessTestSamples.Integrations/Startup.cs
@@ -1,44 +1,38 @@
-using Microsoft.Extensions.DependencyInjection;
 using Amazon.S3;
-using ServerlessTestSamples.Core.Services;
-using ServerlessTestSamples.Core.Queries;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Formatting.Compact;
+using ServerlessTestSamples.Core.Queries;
+using ServerlessTestSamples.Core.Services;
+using System.Text.Json;
 
 namespace ServerlessTestSamples.Integrations;
 
 public static class Startup
 {
-    private static ServiceProvider? _serviceProvider;
+    private static IServiceProvider? _serviceProvider;
 
-    public static ServiceProvider ServiceProvider
+    public static IServiceProvider ServiceProvider => _serviceProvider ??= NewServiceProvider();
+
+    public static void AddDefaultServices(IServiceCollection services)
     {
-        get
-        {
-            if (_serviceProvider == null)
-            {
-                InitializeServiceProvider();
-            }
-
-            return _serviceProvider;
-        }
-        private set => _serviceProvider = value;
-    }
-
-    private static void InitializeServiceProvider()
-    {
-        var services = new ServiceCollection();
-        
         var logger = new LoggerConfiguration()
             .WriteTo.Console(new RenderedCompactJsonFormatter())
             .CreateLogger();
-        
+
         services.AddLogging();
+        services.TryAddSingleton(static sp => Options.Create(new JsonSerializerOptions(JsonSerializerDefaults.Web)));
+        services.TryAddSingleton<IAmazonS3>(static sp => new AmazonS3Client());
+        services.TryAddSingleton<IStorageService, S3StorageService>();
+        services.TryAddSingleton<ListStorageAreasQueryHandler>();
+    }
 
-        services.AddSingleton<IAmazonS3>(new AmazonS3Client());
-        services.AddSingleton<IStorageService, S3StorageService>();
-        services.AddSingleton<ListStorageAreasQueryHandler>();
-
-        _serviceProvider = services.BuildServiceProvider();
+    private static IServiceProvider NewServiceProvider()
+    {
+        var services = new ServiceCollection();
+        AddDefaultServices(services);
+        return services.BuildServiceProvider();
     }
 }
