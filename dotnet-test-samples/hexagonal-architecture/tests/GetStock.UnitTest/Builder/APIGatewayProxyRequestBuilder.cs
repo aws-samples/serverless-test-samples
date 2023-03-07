d420221ef0c0c5403aa94c43e97dad24e45d17a6
Dror Helper <dhelper@amazon.com>
2023-03-07 17:22:23 +0200

    Started testing function code

---
+++ dotnet-test-samples/hexagonal-architecture/tests/GetStock.UnitTest/Builder/APIGatewayProxyRequestBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+
+namespace GetStock.UnitTest.Builder;
+
+internal class APIGatewayProxyRequestBuilder
+{
+    private readonly IDictionary<string, string> _pathParamters = new Dictionary<string, string>();
+
+    public APIGatewayProxyRequest Build()
+    {
+        return new APIGatewayProxyRequest
+        {
+            PathParameters = _pathParamters
+        };
+    }
+
+    public APIGatewayProxyRequestBuilder WithPathParamter(string key, string value)
+    {
+        _pathParamters[key] = value;
+
+        return this;
+    }
+}
