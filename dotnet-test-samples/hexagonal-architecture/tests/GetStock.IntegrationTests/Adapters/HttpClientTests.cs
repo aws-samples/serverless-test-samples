8bd99ee10528c230b99df54b7c8ac2eb7d7db6ab
Dror Helper <dhelper@amazon.com>
2023-03-13 18:39:33 +0200

    Added one more integration test

---
+++ dotnet-test-samples/hexagonal-architecture/tests/GetStock.IntegrationTests/Adapters/HttpClientTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using GetStock.Adapters;
+using GetStock.IntegrationTest.Fixtures;
+using GetStock.IntegrationTest.TestUtilities;
+
+namespace GetStock.IntegrationTest.Adapters
+{
+    public class HttpClientTests : IClassFixture<HttpClientFixture>
+    {
+        static HttpClientTests()
+        {
+            Environment.SetEnvironmentVariable("");
+        }
+
+        public HttpClientTests() : base(
+            new TestServiceConfiguration
+            {
+                CurrencyApiKey = "", //TODO: insert API key from https://fixer.io/
+                CurrencyBaseAddress = "https://api.apilayer.com/fixer/latest"
+            }
+            )
+        {
+        }
+
+        [Fact]
+        public async Task GetCurrencies_passCurrenciesToConvert_ReturnRates()
+        {
+            var target = new CurrencyConverterHttpClient(Client);
+
+            var result = await target.GetCurrencies("USD", new[] { "EUR", "CAD", "GBP" });
+
+            result.Keys.Should().BeEquivalentTo("EUR", "CAD", "GBP");
+        }
+    }
+}
---
+++ dotnet-test-samples/hexagonal-architecture/tests/GetStock.IntegrationTests/Fixtures/HttpClientFixture.cs
@@ -0,0 +1,24 @@
+﻿using GetStock.Adapters;
+using GetStock.IntegrationTest.TestUtilities;
+
+namespace GetStock.IntegrationTest.Fixtures;
+
+public class HttpClientFixture : IDisposable
+{
+    public IHttpClient Client { get; }
+    public HttpClientFixture()
+    {
+        var configuration = new TestServiceConfiguration
+        {
+            CurrencyApiKey = "", //TODO: insert API key from https://fixer.io/
+            CurrencyBaseAddress = "https://api.apilayer.com/fixer/latest"
+        };
+
+        Client = new HttpClientWrapper(configuration);
+    }
+
+    public void Dispose()
+    {
+        Client.Dispose();
+    }
+}
