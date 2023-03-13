98b41d2bd5b24ef2d522de97dfb94d89689cd527
Dror Helper <dhelper@amazon.com>
2023-03-13 13:07:21 +0200

    Added cloud based integration testing

--- dotnet-test-samples/hexagonal-architecture/tests/GetStock.IntegrationTests/Adapters/StockDynamoDbTests.cs
+++ dotnet-test-samples/hexagonal-architecture/tests/GetStock.IntegrationTests/Adapters/StockDynamoDbTests.cs
@@ -1,6 +1,4 @@
-﻿using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DocumentModel;
-using Amazon.DynamoDBv2.Model;
+﻿using Amazon.DynamoDBv2.DocumentModel;
 using GetStock.Adapters;
 using GetStock.Adapters.Exceptions;
 using GetStock.IntegrationTest.Fixtures;
@@ -8,33 +6,11 @@ using GetStock.IntegrationTest.TestUtilities;
 
 namespace GetStock.IntegrationTest.Adapters;
 
-[Collection("Local DynamoDB collection")]
-public class StockDynamoDbTests : IDisposable
+public class StockDynamoDbTests : DynamoDbTestBase
 {
-    private readonly AmazonDynamoDBClient _client;
-    private static readonly string TableName = "test-stocks";
-
-    public StockDynamoDbTests(LocalDynamoDbFixture fixture)
-    {        
-        _client = fixture.Client;
-        var request = new CreateTableRequest
-        {
-            TableName = TableName,
-            AttributeDefinitions = new List<AttributeDefinition>
-            {
-                new("StockId", "S"),
-                //new("Value", "N")
-            },
-            KeySchema = new List<KeySchemaElement> { new("StockId", Amazon.DynamoDBv2.KeyType.HASH) },
-            ProvisionedThroughput = new ProvisionedThroughput { ReadCapacityUnits = 1, WriteCapacityUnits = 1 }
-        };
-
-        _client.CreateTableAsync(request).Wait();
-    }
-
-    public void Dispose()
+    public StockDynamoDbTests(DynamoDbFixture fixture)
+        : base("test-stocks", fixture)
     {
-        _client.DeleteTableAsync(TableName).Wait();
     }
 
     [Fact]
@@ -45,7 +21,7 @@ public class StockDynamoDbTests : IDisposable
             DynamoDbTableName = TableName
         };
 
-        var target = new StockDynamoDb(_client, config);
+        var target = new StockDynamoDb(Client, config);
 
         await Assert.ThrowsAsync<StockNotFoundException>(() => target.GetStockValueAsync("stock-1"));
     }
@@ -61,15 +37,15 @@ public class StockDynamoDbTests : IDisposable
         var item = new Document();
         item["StockId"] = "stock-1";
         item["Value"] = 100.0;
-        
-        var table = Table.LoadTable(_client, TableName);
-                await table.PutItemAsync(item);
 
-        var target = new StockDynamoDb(_client, config);
+        var table = Table.LoadTable(Client, TableName);
+        await table.PutItemAsync(item);
+
+        var target = new StockDynamoDb(Client, config);
 
         var result = await target.GetStockValueAsync("stock-1");
 
-        var expected = new StockData { StockId = "stock-1", Value =100.0 };
+        var expected = new StockData { StockId = "stock-1", Value = 100.0 };
 
         Assert.Equal(expected, result);
     }
---
+++ dotnet-test-samples/hexagonal-architecture/tests/GetStock.IntegrationTests/Fixtures/CloudDynamoDbFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Amazon;
+using Amazon.DynamoDBv2;
+
+namespace GetStock.IntegrationTest.Fixtures
+{
+    public class CloudDynamoDbFixture
+    {
+        public IAmazonDynamoDB Client { get; }
+        private static readonly RegionEndpoint DbRegion = RegionEndpoint.USEast1;
+
+        public CloudDynamoDbFixture()
+        {
+            Client = new AmazonDynamoDBClient(DbRegion);
+        }
+    }
+}
+
