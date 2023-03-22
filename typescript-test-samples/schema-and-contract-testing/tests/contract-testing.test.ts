5a2641f9e7bfd6701d988a7a0e89c262b8c470f0
Katherine Vishnevskaya <kathvish@amazon.com>
2023-03-22 15:08:10 +1100

    Initial commit for schema testing (OpenAPI spec)

---
+++ typescript-test-samples/schema-and-contract-testing/tests/contract-testing.test.ts
@@ -0,0 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+test('received message is equal to 5', async () => {    
+
+    // Compare received message to the expected one
+    expect(5).toEqual(5);
+  });
