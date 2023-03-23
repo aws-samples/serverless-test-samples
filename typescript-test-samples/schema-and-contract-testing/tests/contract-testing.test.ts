bff287daabc2c9d3e692af6a94d472769c8365b2
Katherine Vishnevskaya <kathvish@amazon.com>
2023-03-23 18:09:05 +1100

    added contract testing

--- typescript-test-samples/schema-and-contract-testing/tests/contract-testing.test.ts
+++ typescript-test-samples/schema-and-contract-testing/tests/contract-testing.test.ts
@@ -1,8 +1,51 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-test('received message is equal to 5', async () => {    
+import event_1_0_0 from '../events/event-1.0.0.json';
+import event_1_2_0 from '../events/event-1.2.0.json';
+import event_1_3_0 from '../events/event-1.3.0.json';
 
-    // Compare received message to the expected one
-    expect(5).toEqual(5);
-  });
+// Sample business logic code - we expect to have a full address in the "address" string, e.g. "2 Park St, Sydney, NSW 2000, Australia"
+const isAddressValid = (event) => {
+  if (!event || !event.address) 
+    return false;
+
+  const addressWords = event.address.split(",");
+  if (addressWords.length != 4)
+    return false;
+
+  return true;
+}
+
+test('business logic in the producer has not changed', async () => {    
+  // First check the business logic on the original event.
+  const initialEvent = event_1_0_0;
+  let businessLogicCheck = isAddressValid(initialEvent);
+
+  expect(businessLogicCheck).toBeTruthy();
+
+  // Check the business logic on the new event. 
+  // Since the producer logic has not changed, the address format is still the same.
+  // The business logic check will return the same result.
+  const newEvent = event_1_2_0;
+  businessLogicCheck = isAddressValid(newEvent);
+
+  expect(businessLogicCheck).toBeTruthy();
+});
+
+test('changed business logic in the producer can be caught by a consumer contract test', async () => {    
+  // First check the business logic on the original event.
+  const initialEvent = event_1_0_0;
+  let businessLogicCheck = isAddressValid(initialEvent);
+
+  expect(businessLogicCheck).toBeTruthy();
+
+  // Check the business logic on the new event. 
+  // Since the producer logic has changed, the address will only contain street address now instead of a full address.
+  // The business logic check will return a different result now.
+  const newEvent = event_1_3_0;
+  businessLogicCheck = isAddressValid(newEvent);
+
+  expect(businessLogicCheck).toBeFalsy();
+});
+
