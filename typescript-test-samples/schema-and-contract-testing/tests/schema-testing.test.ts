9fc3fd9847f144fb61f790d47a026b69036309c5
Katherine Vishnevskaya <kathvish@amazon.com>
2023-03-23 14:30:52 +1100

    Replaces OpenAPI schemas with Json v7 schemas

--- typescript-test-samples/schema-and-contract-testing/tests/schema-testing.test.ts
+++ typescript-test-samples/schema-and-contract-testing/tests/schema-testing.test.ts
@@ -1,56 +1,37 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-import openapiDiff from 'openapi-diff';
-import fs from 'fs';
+import jsonSchemaDiff from 'json-schema-diff';
 
-// The schema can be obtained from a repository or Amazon EventBridge Schema Registry;
-// here we're just reading it from a local json file for simplicity
-import schema_v1_0_0 from '../schemas/CustomerCreated-v1.0.0.json';
-import schema_v1_1_0 from '../schemas/CustomerCreated-v1.1.0.json';
-import schema_v1_2_0 from '../schemas/CustomerCreated-v1.2.0.json';
+// The schema can be obtained from a repository or from Amazon EventBridge Schema Registry (the latter supports only OpenAPI 3.0 and Json Schema Draft 4 versions).
+// Here we're just reading schemas from a local json file for simplicity.
+import schema_v1_0_0 from '../schemas/Json/CustomerCreated-v1.0.0.json';
+import schema_v1_1_0 from '../schemas/Json/CustomerCreated-v1.1.0.json';
+import schema_v1_2_0 from '../schemas/Json/CustomerCreated-v1.2.0.json';
 
-test('adding new elements is backward compatible', async () => {    
-
+test('adding new optional elements is backward compatible', async () => {    
   const initialSchema = schema_v1_0_0;
   const updatedSchema = schema_v1_1_0;
 
-  const result = await openapiDiff.diffSpecs({
-    sourceSpec: {
-      content: JSON.stringify(initialSchema),
-      location: 'initialSchema.json',
-      format: 'openapi3'
-    },
-    destinationSpec: {
-      content: JSON.stringify(updatedSchema),
-      location: 'updatedSchema.json',
-      format: 'openapi3'
-    }
+  const result = await jsonSchemaDiff.diffSchemas({
+      sourceSchema: updatedSchema, 
+      destinationSchema: initialSchema
   });
 
-  console.log("result: ", result);
+  const isBreakingChange = result.removalsFound;
 
-  expect(result.breakingDifferencesFound).toBeFalsy();
+  expect(isBreakingChange).toBeFalsy();
 });
 
 test('removing elements is a breaking change', async () => {    
-
   const initialSchema = schema_v1_1_0;
   const updatedSchema = schema_v1_2_0;
 
-  const result = await openapiDiff.diffSpecs({
-    sourceSpec: {
-      content: JSON.stringify(initialSchema),
-      location: 'initialSchema.json',
-      format: 'openapi3'
-    },
-    destinationSpec: {
-      content: JSON.stringify(updatedSchema),
-      location: 'updatedSchema.json',
-      format: 'openapi3'
-    }
+  const result = await jsonSchemaDiff.diffSchemas({
+      sourceSchema: updatedSchema, 
+      destinationSchema: initialSchema
   });
 
-  console.log("result: ", result);
+  const isBreakingChange = result.removalsFound;
 
-  expect(result.breakingDifferencesFound).toBeTruthy();
+  expect(isBreakingChange).toBeTruthy();
 });
