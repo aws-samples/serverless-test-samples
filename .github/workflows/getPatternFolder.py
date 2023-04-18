018dc0407dbf49cf596d8aed8fdd624c43370048
Rohan Mehta <merohan@amazon.com>
2023-04-18 14:00:20 -0400

    Update with Python script for finding directories

--- .github/workflows/getPatternFolder.py
+++ .github/workflows/getPatternFolder.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 # Read the command-line argument passed to the interpreter when invoking the script
-files =os.environ['FILES']
+files = sys.argv[1]
+
 print(files)
 
 fileList= files.split(",")
