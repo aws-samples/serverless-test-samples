7b5351e454ac74efbe1e56abdc996e9351069216
Rohan Mehta <merohan@amazon.com>
2023-04-18 14:18:12 -0400

    Update with Python script for finding directories

--- .github/workflows/getPatternFolder.py
+++ .github/workflows/getPatternFolder.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import github_action_utils as gha_utils
 
 # Read the command-line argument passed to the interpreter when invoking the script
 files = sys.argv[1]
@@ -9,9 +10,12 @@ print(files)
 fileList= files.split(",")
 
 folders = {}
+foldersOutput = ""
 for file in fileList:
     print(file)
     folder = file.split('/')[1].strip()
-    if folder not in folders:
-        folders[folder] = ""
-print(folders.keys())
+    if folder not in foldersOutput:
+        if (len(foldersOutput > 0)):
+            foldersOutput += " "
+        foldersOutput += folder
+gha_utils.set_output("folders", foldersOutput)
