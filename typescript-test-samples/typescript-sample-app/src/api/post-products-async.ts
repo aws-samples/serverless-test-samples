22bff16e56689cfd9b49c2d548c29de5cc5c06b0
Peter Neely <pneely@amazon.com>
2023-05-01 17:43:35 -0500

    Add logging, tracing, and metrics

--- typescript-test-samples/typescript-sample-app/src/api/post-products-async.ts
+++ typescript-test-samples/typescript-sample-app/src/api/post-products-async.ts
@@ -1,14 +1,19 @@
+import middy from '@middy/core';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { MetaProduct } from '../model/Product';
+import { MetricUnits, logMetrics } from '@aws-lambda-powertools/metrics';
 import { SQSClient, SendMessageBatchCommand, SendMessageBatchCommandInput, SendMessageBatchRequestEntry } from '@aws-sdk/client-sqs';
-import { v4 as uuid } from 'uuid';
+import { captureLambdaHandler } from '@aws-lambda-powertools/tracer';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger';
+import { logger, tracer, metrics } from "../powertools/utilities"
 import { shortTimestamp } from '../utils.ts/dates';
-import { formatJson } from '../utils.ts/strings';
+import { v4 as uuid } from 'uuid';
+
 
 const sqsClient = new SQSClient({});
 
-const sendBatchedProducts = async (batchedProducts: MetaProduct[]): Promise<null | unknown> => {
-  const entries: SendMessageBatchRequestEntry[] = batchedProducts.map((product) => {
+const sendProducts = async (products: MetaProduct[]): Promise<null | unknown> => {
+  const entries: SendMessageBatchRequestEntry[] = products.map((product) => {
     product.timeQueued = shortTimestamp();
     return {
       Id: uuid(),
@@ -25,32 +30,43 @@ const sendBatchedProducts = async (batchedProducts: MetaProduct[]): Promise<null
   try {
     const data = await sqsClient.send(new SendMessageBatchCommand(sendMessageParams));
     if (data) {
-      console.log('Success, message sent.', formatJson(data));
+      logger.info('Success, message sent.', { data });
+      metrics.addMetric('productsSent', MetricUnits.Count, products.length);
     }
     return null; // no error
   } catch (err) {
-    console.log('Error', err);
+    logger.error('Error', { error: err });
     return err;
   }
 };
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const products = JSON.parse(event.body ?? '[]') as MetaProduct[];
+const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.appendKeys({
+    resource_path: event.requestContext.resourcePath
+  });
+
+  let products = JSON.parse(event.body ?? '[]') as MetaProduct[];
+  let errorPromises = [];
 
   while (products.length) {
     const batchedProducts = products.splice(0, 10); // max batch size is 10 for SQS
-
-    const error = await sendBatchedProducts(batchedProducts);
-    if (error) {
-      return {
-        statusCode: 500,
-        body: formatJson('Some error occurred '),
-      };
-    }
+    const error = await sendProducts(batchedProducts);
+    errorPromises.push(error);
   }
 
-  return {
-    statusCode: 201,
-    body: formatJson({ message: `${products.length} products queued` }),
-  };
+  const errors = await Promise.all(errorPromises);
+  return errors.some((error) => error)
+    ? {
+        statusCode: 500,
+        body: JSON.stringify('Some error occurred '),
+      }
+    : {
+        statusCode: 201,
+        body: JSON.stringify({ message: `${products.length} products queued` }),
+      };
 };
+
+export const handler = middy(lambdaHandler)
+    .use(captureLambdaHandler(tracer))
+    .use(logMetrics(metrics, { captureColdStartMetric: true }))
+    .use(injectLambdaContext(logger, { clearState: true }));
