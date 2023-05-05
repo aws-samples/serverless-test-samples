85a070a13ce3385b9b004ac18754178e0184f441
Brian Krygsman <krygsman@amazon.com>
2023-05-05 16:28:37 -0400

    Add copyright lines

--- typescript-test-samples/lambda-handler-dynamodb/bin/lambda-handler-dynamodb.ts
+++ typescript-test-samples/lambda-handler-dynamodb/bin/lambda-handler-dynamodb.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+
+//Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//SPDX-License-Identifier: MIT-0
+
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { LambdaHandlerDynamodbStack } from '../lib/lambda-handler-dynamodb-stack';
--- typescript-test-samples/lambda-handler-dynamodb/lib/lambda-handler-dynamodb-stack.ts
+++ typescript-test-samples/lambda-handler-dynamodb/lib/lambda-handler-dynamodb-stack.ts
@@ -1,3 +1,6 @@
+//Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//SPDX-License-Identifier: MIT-0
+
 import * as cdk from 'aws-cdk-lib';
 import { Duration, RemovalPolicy } from 'aws-cdk-lib';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
