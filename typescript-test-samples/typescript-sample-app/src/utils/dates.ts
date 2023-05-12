0ca4489544fb36a4de9ea98a60f680ff01b3ee75
Peter Neely <pneely@amazon.com>
2023-05-12 17:41:38 -0500

    fix merge issues

--- typescript-test-samples/typescript-sample-app/src/utils/dates.ts
+++ typescript-test-samples/typescript-sample-app/src/utils/dates.ts
@@ -1,7 +1,6 @@
-import dayjs from 'dayjs'
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-
+import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(utc);
