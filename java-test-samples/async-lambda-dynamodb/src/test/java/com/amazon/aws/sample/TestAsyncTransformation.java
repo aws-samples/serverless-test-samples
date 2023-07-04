4f7b50aae557fc4e278c9d59a102a160ba82c8f7
Gustavo Martim <gmartim@amazon.com>
2023-07-04 06:23:33 -0300

    Adding library to handle the retry

--- java-test-samples/async-lambda-dynamodb/src/test/java/com/amazon/aws/sample/TestAsyncTransformation.java
+++ java-test-samples/async-lambda-dynamodb/src/test/java/com/amazon/aws/sample/TestAsyncTransformation.java
@@ -1,17 +1,23 @@
 package com.amazon.aws.sample;
 
+import static io.github.resilience4j.core.IntervalFunction.ofExponentialBackoff;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Callable;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
+import io.github.resilience4j.core.IntervalFunction;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
@@ -27,6 +33,74 @@ import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+class RecordNotFoundException extends Exception {
+
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+}
+
+class InternalErrorException extends Exception {
+
+    public InternalErrorException(String message) {
+        super(message);
+    }
+
+    public InternalErrorException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+}
+
+class GetItemCallable implements Callable<String> {
+
+    private DynamoDbClient dynamoDbClient;
+
+    private String tableName;
+
+    private String key;
+
+    public GetItemCallable(DynamoDbClient dynamoDbClient, String tableName, String key) {
+        this.dynamoDbClient = dynamoDbClient;
+        this.tableName = tableName;
+        this.key = key;
+    }
+
+    @Override
+    public String call() throws Exception {
+        Map<String, AttributeValue> keyAttributeValues = new HashMap<>();
+        keyAttributeValues.put("id", AttributeValue.builder().s(key).build());
+
+        GetItemRequest getItemRequest = GetItemRequest.builder().tableName(tableName)
+                .key(keyAttributeValues).build();
+
+        GetItemResponse getItemResponse;
+
+        try {
+            getItemResponse = dynamoDbClient.getItem(getItemRequest);
+        } catch (DynamoDbException dynamoDbException) {
+            throw new InternalErrorException("Error on get item", dynamoDbException);
+        }
+
+        if (!getItemResponse.hasItem()) {
+            throw new RecordNotFoundException("Record not found");
+        }
+
+        Map<String, AttributeValue> itemAttributeValues = getItemResponse.item();
+
+        if (itemAttributeValues.isEmpty() ||
+                !itemAttributeValues.containsKey("content")) {
+            throw new InternalErrorException("Error on get item");
+        }
+
+        String content = itemAttributeValues.get("content").s();
+
+        return content;
+    }
+
+}
 
 @EnabledIfEnvironmentVariable(named = "AWS_SAM_STACK_NAME", matches = ".*")
 public class TestAsyncTransformation {
@@ -89,15 +163,21 @@ public class TestAsyncTransformation {
         assertNotNull(putObjectResponse, "putObjectResponse may not be null");
 
         if (recordTransformationTable != null) {
-            String contentFromRecordTransformationTable = getContentFromDynamoDB(recordTransformationTable, key);
-            assertEquals(MESSAGE_TO_UPPER_CASE, contentFromRecordTransformationTable,
-                    String.format("contentFromRecordTransformationTable must be %s instead of %s",
-                            MESSAGE_TO_UPPER_CASE, contentFromRecordTransformationTable));
+            try {
+                Callable<String> callable = getContentFromDynamoDB(recordTransformationTable, key);
+
+                String contentFromRecordTransformationTable = callable.call();
+
+                assertEquals(contentFromRecordTransformationTable, contentFromRecordTransformationTable,
+                        String.format("contentFromRecordTransformationTable must be %s instead of %s",
+                                MESSAGE_TO_UPPER_CASE, contentFromRecordTransformationTable));
+            } catch (Exception exception) {
+                fail("An exception occurred and should not have");
+            }
         }
     }
 
     @Test
-    // TODO: check the thrown exception when the item is not found
     public void testAsyncTransformation_when_not_txt() {
         String key = String.format("%s.not_txt", UUID.randomUUID().toString());
 
@@ -108,59 +188,29 @@ public class TestAsyncTransformation {
         assertNotNull(putObjectResponse, "putObjectResponse may not be null");
 
         if (recordTransformationTable != null) {
-            String contentFromRecordTransformationTable = getContentFromDynamoDB(recordTransformationTable, key);
-            assertTrue(contentFromRecordTransformationTable == null,
-                    String.format("contentFromRecordTransformationTable must be null instead of %s",
-                            contentFromRecordTransformationTable));
+            try {
+                Callable<String> callable = getContentFromDynamoDB(recordTransformationTable, key);
+
+                callable.call();
+
+                fail("It not reach here, an exception should be thrown");
+            } catch (Exception exception) {
+                assertTrue((exception instanceof RecordNotFoundException), "An exception occured and shoud not have");
+            }
         }
     }
 
-    // TODO: check about usage of a retry library
-    // TODO: throw an exception instead of return null when it is not found
-    private String getContentFromDynamoDB(String tableName, String key) {
-        int numberOfRetries = 0;
+    private Callable<String> getContentFromDynamoDB(String tableName, String key)
+            throws InternalErrorException, RecordNotFoundException {
+        IntervalFunction intervalFunction = ofExponentialBackoff(3000l, 1.5d);
 
-        String content = null;
+        RetryConfig retryConfig = RetryConfig.custom().maxAttempts(3).intervalFunction(intervalFunction)
+                .retryExceptions(RecordNotFoundException.class).build();
+        Retry retry = Retry.of("dynamoDbClient.getItem", retryConfig);
 
-        while (numberOfRetries < 3) {
-            try {
-                Thread.sleep(5 * 1000);
-            } catch (InterruptedException interruptedException) {
-                break;
-            }
+        Callable<String> getItemCallable = new GetItemCallable(dynamoDbClient, tableName, key);
 
-            numberOfRetries++;
-
-            Map<String, AttributeValue> keyAttributeValues = new HashMap<>();
-            keyAttributeValues.put("id", AttributeValue.builder().s(key).build());
-
-            GetItemRequest getItemRequest = GetItemRequest.builder().tableName(recordTransformationTable)
-                    .key(keyAttributeValues).build();
-
-            GetItemResponse getItemResponse;
-
-            try {
-                getItemResponse = dynamoDbClient.getItem(getItemRequest);
-            } catch (DynamoDbException dynamoDbException) {
-                break;
-            }
-
-            if (!getItemResponse.hasItem()) {
-                continue;
-            }
-
-            Map<String, AttributeValue> itemAttributeValues = getItemResponse.item();
-
-            if (itemAttributeValues.isEmpty()) {
-                continue;
-            }
-
-            content = itemAttributeValues.get("content").s();
-
-            break;
-        }
-
-        return content;
+        return Retry.decorateCallable(retry, getItemCallable);
     }
 
 }
