6f087328b58045681800d44cb9f38127909dd679
jamesuk <jamesuk@amazon.com>
2023-12-12 10:31:45 +0000

    Remove basic integration tests

--- dotnet-test-samples/test-containers/tests/ApiTests.IntegrationTest/Drivers/ProductWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiTests.IntegrationTest.Drivers
-{
-    public class ProductWrapper
-    {
-        public ProductWrapper(List<Product> products) => Products = products;
-
-        public List<Product> Products { get; }
-    }
-}
