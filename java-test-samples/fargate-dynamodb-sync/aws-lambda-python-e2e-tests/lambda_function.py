32edce55bdd11a46582bab4f059fb6916260d545
arunasha9 <ashanubhogue@deloitte.com>
2024-02-12 19:31:12 +0530

    update integration test lambda file

---
+++ java-test-samples/fargate-dynamodb-sync/aws-lambda-python-e2e-tests/lambda_function.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import boto3
+import create_test
+import read_test
+import delete_test
+import update_test
+import pytest
+import os
+import subprocess
+ 
+print('Loading function')
+s3 = boto3.client('s3')
+s3_bucket = os.environ.get('S3_BUCKET', 'pocserverless-uc1')
+report_name = 'pytest_report.html'
+logToUpload = {}
+ 
+def lambda_handler(event, context):
+logToUpload['GET'] = read_test.test_get_list_of_users()
+logToUpload['POST'] = create_test.test_create_new_user()
+logToUpload['DELETE'] = delete_test.test_delete_user()
+logToUpload['PUT'] = update_test.test_update_user()
+ 
+# Upload the result file into S3 bucket
+fileName = 'pocserverless_test_report' + '.json'
+uploadByteStream = bytes(json.dumps(logToUpload).encode('UTF-8'))
+s3.put_object(Bucket=s3_bucket, Key=fileName, Body=uploadByteStream)
+print('Put Complete')  
+# Generate HTML report
+subprocess.run(["/opt/python -m pytest --html=/var/lang/bin/report.html"], shell=True, capture_output=True, text=True)
+s3.upload_file('/var/lang/bin/report.html', s3_bucket, report_name)
--- java-test-samples/fargate-dynamodb-sync/aws-lambda-python-e2e-tests/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print('Loading function')
-def lambda_handler():
-    os.system("pytest --html=report.html --self-contained-html")
-
-if __name__ == "__main__":
-    lambda_handler()
