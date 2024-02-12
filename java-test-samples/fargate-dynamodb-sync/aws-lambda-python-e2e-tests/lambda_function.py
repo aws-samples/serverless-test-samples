42ac5d5a95eb28bc225346fb25ac69e942115721
arunasha9 <ashanubhogue@deloitte.com>
2024-02-12 19:55:01 +0530

    Update lambda_function.py

--- java-test-samples/fargate-dynamodb-sync/aws-lambda-python-e2e-tests/lambda_function.py
+++ java-test-samples/fargate-dynamodb-sync/aws-lambda-python-e2e-tests/lambda_function.py
@@ -21,11 +21,12 @@ def lambda_handler(event, context):
     logToUpload['DELETE'] = delete_test.test_delete_user()
     logToUpload['PUT'] = update_test.test_update_user()
  
-# Upload the result file into S3 bucket
-fileName = 'pocserverless_test_report' + '.json'
-uploadByteStream = bytes(json.dumps(logToUpload).encode('UTF-8'))
-s3.put_object(Bucket=s3_bucket, Key=fileName, Body=uploadByteStream)
-print('Put Complete')  
-# Generate HTML report
-subprocess.run(["/opt/python -m pytest --html=/var/lang/bin/report.html"], shell=True, capture_output=True, text=True)
-s3.upload_file('/var/lang/bin/report.html', s3_bucket, report_name)
+    # Upload the result file into S3 bucket
+    fileName = 'pocserverless_test_report' + '.json'
+    uploadByteStream = bytes(json.dumps(logToUpload).encode('UTF-8'))
+    s3.put_object(Bucket=s3_bucket, Key=fileName, Body=uploadByteStream)
+    print('Put Complete') 
+     
+    # Generate HTML report
+    subprocess.run(["/opt/python -m pytest --html=/var/lang/bin/report.html"], shell=True, capture_output=True, text=True)
+    s3.upload_file('/var/lang/bin/report.html', s3_bucket, report_name)
